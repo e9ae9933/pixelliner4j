ad656b217ab94a44168eb2ff23eefb4fc24ab2e3
example <example@example.com>
2026-04-07 00:07:45 +0800

    fix NoelByteBuffer

--- src/main/java/org/aliceincradle/pxlsloader/NoelByteBuffer.java
+++ src/main/java/org/aliceincradle/pxlsloader/NoelByteBuffer.java
@@ -1,181 +1,184 @@
 package org.aliceincradle.pxlsloader;
 
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.Arrays;
 
 public class NoelByteBuffer {
-	
-	public NoelByteBuffer() {
-		this(new byte[0]);
-	}
-	
-	public NoelByteBuffer(byte[] bytes) {
-		buf=new ArrayDeque<>();
-		for(byte b:bytes)
-			buf.addLast(b);
-	}
-	
-	private final Deque<Byte> buf;
-	
-	public int size() {
-		return buf.size();
-	}
-	
-	public byte getByte() {
-		return buf.removeFirst();
-	}
-	
-	public void putByte(byte b) {
-		buf.addLast(b);
-	}
-	
-	public void end() {
-		if (size()>0) {
-			System.err.println("Warning: left "+size()+" byte(s)");
-			List<Byte> l=new ArrayList<>();
-			for (int i=0; size()>0&&i<32; i++)
-				l.add(getByte());
-			System.err.println("printing first "+l.size()+" bytes");
-			for (Byte b : l)
-				System.err.printf("%02X ", b);
-			System.err.println();
-			for (Byte b : l)
-				System.err.printf("%s ",
-				                  Character.isISOControl(b) ? ".." : " "+(char) b.byteValue());
-			System.err.println();
-			//			data=null;
-		}
-	}
-	
-	public short getShort() {
-		return (short) ((getByte()&0xFF)<<8|getByte()&0xFF);
-	}
-	
-	public void putShort(short s) {
-		putByte((byte) (s >>> 8));
-		putByte(((byte) s));
-	}
-	
-	public int getInt() {
-		return (getShort()&0xFFFF)<<16|getShort()&0xFFFF;
-	}
-	
-	public void putInt(int i) {
-		putShort((short) (i >>> 16));
-		putShort((short) i);
-	}
-	
-	public long getLong() {
-		return (getInt()&0xFFFFFFFFL)<<32|getInt()&0xFFFFFFFFL;
-	}
-	
-	public void putLong(long l) {
-		putInt((int) (l >>> 32));
-		putInt((int) l);
-	}
-	
-	public float getFloat() {
-		return Float.intBitsToFloat(getInt());
-	}
-	
-	public void putFloat(float f) {
-		putInt(Float.floatToRawIntBits(f));
-	}
-	
-	public double getDouble() {
-		return Double.longBitsToDouble(getLong());
-	}
-	
-	public void putDouble(double d) {
-		putLong(Double.doubleToRawLongBits(d));
-	}
-	
-	public void getBytes(byte[] b) {
-		for (int i=0; i<b.length; i++)
-			b[i]=getByte();
-	}
-	
-	public byte[] getNBytes(int len) {
-		byte[] b=new byte[len];
-		getBytes(b);
-		return b;
-	}
-	
-	public byte[] getAllBytes() {
-		return getNBytes(size());
-	}
-	
-	public void putBytes(byte[] b) {
-		for (int i=0; i<b.length; i++)
-			putByte(b[i]);
-	}
-	
-	public boolean getBoolean() {
-		return getByte()!=0;
-	}
-	
-	public String getUTFString() {
-		return new String(getNBytes(Short.toUnsignedInt(getShort())), StandardCharsets.UTF_8);
-	}
-	
-	public short getUnsignedByte() {
-		return (short) (getByte()&0xFF);
-	}
-	
-	public int getUnsignedShort() {
-		return Short.toUnsignedInt(getShort());
-	}
-	
-	public long getUnsignedInt() {
-		return getInt()&0xFFFFFFFFL;
-	}
-	
-	public PixelLinerKey getPixelLinerKey() {
-		int id=getInt();
-		double id2=getDouble();
-		return new PixelLinerKey(id,id2);
-	}
-	public void putPixelLinerKey(PixelLinerKey key) {
-		putInt(key.id);
-		putDouble(key.id2);
-	}
-	
-	public NoelByteBuffer getSegment() {
-		int len=getInt();
-		return new NoelByteBuffer(getNBytes(len));
-	}
-	
-	public void putUTFString(String s) {
-		putShort((short) s.length());
-		putBytes(s.getBytes(StandardCharsets.UTF_8));
-	}
-	public void putRawString(String s) {
-		putBytes(s.getBytes(StandardCharsets.UTF_8));
-	}
-	
-	public void putBoolean(boolean b) {
-		putByte(b ? (byte) 1 : (byte) 0);
-	}
-	
-	public void putSegment(NoelByteBuffer b) {
-		putSegment(b.getAllBytes());
-	}
-	
-	public void putSegment(byte[] data) {
-		putInt(data.length);
-		putBytes(data);
-	}
-	
-	public String getLengthedString(int len) {
-		return new String(getNBytes(len),StandardCharsets.UTF_8);
-	}
-	
-//	@Override
-//	@SuppressWarnings("removal")
-//	protected void finalize() throws Throwable {
-//		this.end();
-//	}
+    private byte[] buf;
+    private int readPos;
+    private int writePos;
+    
+    public NoelByteBuffer() {
+        this.buf = new byte[1024];
+        this.readPos = 0;
+        this.writePos = 0;
+    }
+    
+    public NoelByteBuffer(byte[] bytes) {
+        this.buf = bytes;
+        this.readPos = 0;
+        this.writePos = bytes.length;
+    }
+    
+    private void ensureCapacity(int need) {
+        if (writePos + need > buf.length) {
+            int newCap = Math.max(buf.length * 2, writePos + need);
+            buf = Arrays.copyOf(buf, newCap);
+        }
+    }
+    
+    public int size() {
+        return writePos - readPos;
+    }
+    
+    public byte getByte() {
+        return buf[readPos++];
+    }
+    
+    public void putByte(byte b) {
+        ensureCapacity(1);
+        buf[writePos++] = b;
+    }
+    
+    public short getShort() {
+        return (short) (((buf[readPos++] & 0xFF) << 8) | (buf[readPos++] & 0xFF));
+    }
+    
+    public void putShort(short s) {
+        ensureCapacity(2);
+        buf[writePos++] = (byte) (s >>> 8);
+        buf[writePos++] = (byte) s;
+    }
+    
+    public int getInt() {
+        return ((buf[readPos++] & 0xFF) << 24) |
+               ((buf[readPos++] & 0xFF) << 16) |
+               ((buf[readPos++] & 0xFF) << 8)  |
+               ((buf[readPos++] & 0xFF));
+    }
+    
+    public void putInt(int i) {
+        ensureCapacity(4);
+        buf[writePos++] = (byte) (i >>> 24);
+        buf[writePos++] = (byte) (i >>> 16);
+        buf[writePos++] = (byte) (i >>> 8);
+        buf[writePos++] = (byte) i;
+    }
+    
+    public long getLong() {
+        return ((long) getInt() << 32) | (getInt() & 0xFFFFFFFFL);
+    }
+    
+    public void putLong(long l) {
+        putInt((int) (l >>> 32));
+        putInt((int) l);
+    }
+    
+    public float getFloat() {
+        return Float.intBitsToFloat(getInt());
+    }
+    
+    public void putFloat(float f) {
+        putInt(Float.floatToRawIntBits(f));
+    }
+    
+    public double getDouble() {
+        return Double.longBitsToDouble(getLong());
+    }
+    
+    public void putDouble(double d) {
+        putLong(Double.doubleToRawLongBits(d));
+    }
+    
+    public void getBytes(byte[] b) {
+        System.arraycopy(buf, readPos, b, 0, b.length);
+        readPos += b.length;
+    }
+    
+    public byte[] getNBytes(int len) {
+        byte[] b = new byte[len];
+        getBytes(b);
+        return b;
+    }
+    
+    public byte[] getAllBytes() {
+        return Arrays.copyOfRange(buf, readPos, writePos);
+    }
+    
+    public void putBytes(byte[] b) {
+        ensureCapacity(b.length);
+        System.arraycopy(b, 0, buf, writePos, b.length);
+        writePos += b.length;
+    }
+    
+    public boolean getBoolean() {
+        return getByte() != 0;
+    }
+    
+    public void putBoolean(boolean b) {
+        putByte(b ? (byte) 1 : (byte) 0);
+    }
+    
+    public void putUTFString(String s) {
+        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        putShort((short) bytes.length);
+        putBytes(bytes);
+    }
+    
+    public String getUTFString() {
+        int len = Short.toUnsignedInt(getShort());
+        String s = new String(buf, readPos, len, StandardCharsets.UTF_8);
+        readPos += len;
+        return s;
+    }
+    
+    public void putRawString(String s) {
+        putBytes(s.getBytes(StandardCharsets.UTF_8));
+    }
+    
+    public String getLengthedString(int len) {
+        String s = new String(buf, readPos, len, StandardCharsets.UTF_8);
+        readPos += len;
+        return s;
+    }
+    
+    public short getUnsignedByte() {
+        return (short) (getByte() & 0xFF);
+    }
+    
+    public int getUnsignedShort() {
+        return Short.toUnsignedInt(getShort());
+    }
+    
+    public long getUnsignedInt() {
+        return getInt() & 0xFFFFFFFFL;
+    }
+    
+    public PixelLinerKey getPixelLinerKey() {
+        return new PixelLinerKey(getInt(), getDouble());
+    }
+    
+    public void putPixelLinerKey(PixelLinerKey key) {
+        putInt(key.id);
+        putDouble(key.id2);
+    }
+    
+    public NoelByteBuffer getSegment() {
+        return new NoelByteBuffer(getNBytes(getInt()));
+    }
+    
+    public void putSegment(NoelByteBuffer b) {
+        putSegment(b.getAllBytes());
+    }
+    
+    public void putSegment(byte[] data) {
+        putInt(data.length);
+        putBytes(data);
+    }
+    
+    public void end() {
+        if (size() > 0) {
+            System.err.println("Warning: left " + size() + " byte(s)");
+        }
+    }
 }
